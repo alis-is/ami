33ae4b61d6d893605c7912d88353c4af45858d68
cryi <cryi@tutanota.com>
2021-01-21 15:31:09 +0100

    removed unecessary GLOBAL_LOGGER initialization

--- src/ami/init.lua
+++ src/ami/init.lua
@@ -4,7 +4,6 @@ local function _init(...)
     am = util.merge_tables(require"version-info", require "ami.am"(...))
     hjson = util.generate_safe_functions(require "hjson")
 
-    GLOBAL_LOGGER = Logger:new()
     log_success, log_trace, log_debug, log_info, log_warn, log_error = util.global_log_factory("ami", "success", "trace", "debug", "info", "warn", "error")
 
     ami_error = ami_error or function (msg, exitCode)
